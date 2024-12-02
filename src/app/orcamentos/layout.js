dab47637238cc46b7737e5bdc9589eb47b56fed5
fhok2 <138731655+fhok2@users.noreply.github.com>
2024-12-02 14:01:22 -0300

    adicona link whatsaap

---
+++ src/app/orcamentos/layout.js
@@ -0,0 +1,19 @@
+
+
+
+export const metadata = {
+    title: "Orcamentos",
+    description: "Orcamento de serviços",
+  };
+  
+  export default function RootLayout({ children }) {
+    return (
+      <html lang="pt-br">
+        <body
+        >
+          {children}
+        </body>
+      </html>
+    );
+  }
+  
---
+++ src/app/orcamentos/michele/layout.js
@@ -0,0 +1,18 @@
+
+
+
+export const metadata = {
+  title: "Orcamento Michele",
+  description: "Orcamento de serviços de Michele",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
